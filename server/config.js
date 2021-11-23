be3b27dd3ef97ce6fe562d807fd93884a0268c6c
Abdul Samad <abdul.samadkirmani.samad63@gmail.com>
2021-11-23 22:18:49 +0530

    Database configurations

---
+++ server/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB : "mongodb+srv://classess:KbV1FwBElEpNeN57@cluster0.ffdf2.mongodb.net/MERNG?retryWrites=true&w=majority",    
+    SECRET_KEY : 'someverysecretkey'
+}
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const {MONGODB} = require('./config');
 const {json, Router} = require('express');
 
 const educationController = require('./Controllers/EducationController');
@@ -12,3 +13,11 @@ app.use("/education", educationController);
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
 });
+
+mongoose.connect(MONGODB, {useNewUrlParser : true})
+    .then(() => {
+        console.log(`MongoDB Connected`)
+    })
+    .catch(err => {
+        console.log(`Error is ${err}`)
+    });
